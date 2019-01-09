e7d654791c72d7e0aff441ac25bc4a95693dd337
TimMaddox <att_maddox@hotmail.com>
2019-01-09 08:24:56 -0600

    Add files via upload

---
+++ src/aura/gearsDemo/gearsDemoController.js
@@ -0,0 +1,22 @@
+(
+    {
+        handleInit : function(component, event, helper) {        
+            helper.getGears(component);
+        },
+        handleSelectChange : function(component, event, helper) {
+            helper.setGearRatio(component);  
+        },
+        setChainRing: function(component, event, helper)
+        {
+            helper.chainRing(component, event);    
+        },
+        setUpOrDown: function(component, event, helper)
+        {	
+            helper.upOrDown(component, event);
+        },
+        executeShift : function(component, event, helper)
+        {
+          helper.processShiftRequest(component, event);  
+        }
+	}
+)
